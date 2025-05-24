2b462f3e1c222ce819a0dfdd4b7e90a65ddf6878
Baesukju <ssmm8259@gmail.com>
2025-05-24 19:23:54 +0900

    add compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    // 이미지 경로 설정
+    setImageUrl('/api/animal');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {imageUrl ? (
+          <img src={imageUrl} alt="animal" style={{ width: '300px' }} />
+        ) : (
+          <p>Loading image...</p>
+        )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
